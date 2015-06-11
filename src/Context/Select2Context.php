9d0690240d8fcc412a3037b6aa6046aeb54e2ae5
Tanya Misheva <tanya.misheva@gmail.com>
2015-06-11 12:28:27 +0100

    Select2Context - Fix for Select2 version 3

--- src/Context/Select2Context.php
+++ src/Context/Select2Context.php
@@ -4,10 +4,16 @@ namespace Novaway\CommonContexts\Context;
 
 use Behat\Mink\Element\DocumentElement;
 
+/**
+ * Defines application features from Select2 context.
+ */
 class Select2Context extends BaseContext
 {
     /**
      * Fills in Select2 field with specified
+     *
+     * @param string $field
+     * @param string $value
      *
      * @When /^(?:|I )fill in select2 "(?P<field>(?:[^"]|\\")*)" with "(?P<value>(?:[^"]|\\")*)"$/
      * @When /^(?:|I )fill in select2 "(?P<value>(?:[^"]|\\")*)" for "(?P<field>(?:[^"]|\\")*)"$/
@@ -15,7 +21,6 @@ class Select2Context extends BaseContext
     public function iFillInSelect2Field($field, $value)
     {
         $page = $this->getSession()->getPage();
-
         $this->openField($page, $field);
         $this->selectValue($page, $field, $value);
     }
@@ -23,12 +28,14 @@ class Select2Context extends BaseContext
     /**
      * Fill Select2 input field
      *
+     * @param string $field
+     * @param string $value
+     *
      * @When /^(?:|I )fill in select2 input "(?P<field>(?:[^"]|\\")*)" with "(?P<value>(?:[^"]|\\")*)"$/
      */
     public function iFillInSelect2InputWith($field, $value)
     {
         $page = $this->getSession()->getPage();
-
         $this->openField($page, $field);
         $this->fillSearchField($page, $field, $value);
     }
@@ -36,12 +43,15 @@ class Select2Context extends BaseContext
     /**
      * Fill Select2 input field and select a value
      *
+     * @param string $field
+     * @param string $value
+     * @param string $entry
+     *
      * @When /^(?:|I )fill in select2 input "(?P<field>(?:[^"]|\\")*)" with "(?P<value>(?:[^"]|\\")*)" and select "(?P<entry>(?:[^"]|\\")*)"$/
      */
     public function iFillInSelect2InputWithAndSelect($field, $value, $entry)
     {
         $page = $this->getSession()->getPage();
-
         $this->openField($page, $field);
         $this->fillSearchField($page, $field, $value);
         $this->selectValue($page, $field, $entry);
@@ -50,12 +60,14 @@ class Select2Context extends BaseContext
     /**
      * Fill Select2 input field
      *
+     * @param string $field
+     * @param int    $num
+     *
      * @Then /^(?:|I )should see (?P<num>\d+) choice(?:|s) in select2 "(?P<field>(?:[^"]|\\")*)"$/
      */
     public function iShouldSeeSelectChoices($field, $num)
     {
         $selector = sprintf('#select2-%s-results li', $field);
-
         $this->assertSession()->elementsCount('css', $selector, intval($num));
     }
 
@@ -64,18 +76,18 @@ class Select2Context extends BaseContext
      *
      * @param DocumentElement $page
      * @param string          $field
+     *
      * @throws \Exception
      */
     private function openField(DocumentElement $page, $field)
     {
-        $fieldName = sprintf('#select2-%s-container', $field);
-
+        //$fieldName = sprintf('#select2-%s-container', $field);
+        $fieldName = sprintf('#s2id_%s', $field);
         $inputField = $page->find('css', $fieldName);
         if (!$inputField) {
             throw new \Exception(sprintf('No field "%s" found', $field));
         }
-
-        $choice = $inputField->getParent()->find('css', '.select2-selection');
+        $choice = $inputField->getParent()->find('css', '.select2-choice');
         if (!$choice) {
             throw new \Exception(sprintf('No select2 choice found for "%s"', $field));
         }
@@ -88,6 +100,7 @@ class Select2Context extends BaseContext
      * @param DocumentElement $page
      * @param string          $field
      * @param string          $value
+     *
      * @throws \Exception
      */
     private function fillSearchField(DocumentElement $page, $field, $value)
@@ -95,28 +108,27 @@ class Select2Context extends BaseContext
         $driver = $this->getSession()->getDriver();
         if ('Behat\Mink\Driver\Selenium2Driver' === get_class($driver)) {
             // Can't use `$this->getSession()->getPage()->find()` because of https://github.com/minkphp/MinkSelenium2Driver/issues/188
-            $select2Input = $this->getSession()->getDriver()->getWebDriverSession()->element('xpath', "//html/descendant-or-self::*[@class and contains(concat(' ', normalize-space(@class), ' '), ' select2-search__field ')]");
+            $select2Input = $this->getSession()->getDriver()->getWebDriverSession()->element('xpath', "//html/descendant-or-self::*[@class and contains(concat(' ', normalize-space(@class), ' '), ' select2-input ')]");
             if (!$select2Input) {
                 throw new \Exception(sprintf('No field "%s" found', $field));
             }
             $select2Input->postValue(['value' => [$value]]);
         } else {
-            $select2Input = $page->find('css', '.select2-search__field');
+            $select2Input = $page->find('css', '.select2-input');
             if (!$select2Input) {
                 throw new \Exception(sprintf('No input found for "%s"', $field));
             }
             $select2Input->setValue($value);
         }
-
         $this->getSession()->wait(10000, '(0 === jQuery.active)');
     }
-
     /**
      * Select value in choice list
      *
      * @param DocumentElement $page
      * @param string          $field
      * @param string          $value
+     *
      * @throws \Exception
      */
     private function selectValue(DocumentElement $page, $field, $value)
@@ -125,10 +137,10 @@ class Select2Context extends BaseContext
         foreach ($chosenResults as $result) {
             if ($result->getText() == $value) {
                 $result->click();
+
                 return;
             }
         }
-
         throw new \Exception(sprintf('Value "%s" not found for "%s"', $value, $field));
     }
 }
